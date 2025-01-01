7145fa0ef0aa10b43e5bac12767a78a499d78860
minominolyly <minominolyly@users.noreply.github.com>
2025-01-01 19:24:04 +0000

    deploy: 4b3ac820aca3b87fccdf87d9c9cf3944745a89a1

--- assets/entry-preview-CX7pGz4p.js
+++ assets/entry-preview-CX7pGz4p.js
@@ -1,5 +1,5 @@
 const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./react-18-BmP0M55Z.js","./index-CqrSl2Gl.js"])))=>i.map(i=>d[i]);
-import{_ as Se}from"./iframe-Cv7Ubp7q.js";import{_ as Te,a as ge,b as _}from"./chunk-XP5HYGXS-BGCqD1aY.js";import{a as Ae,R as W,r as H}from"./index-CqrSl2Gl.js";import"../sb-preview/runtime.js";var ne={exports:{}},le={};/**
+import{_ as Se}from"./iframe-BP9qQ0lK.js";import{_ as Te,a as ge,b as _}from"./chunk-XP5HYGXS-BGCqD1aY.js";import{a as Ae,R as W,r as H}from"./index-CqrSl2Gl.js";import"../sb-preview/runtime.js";var ne={exports:{}},le={};/**
  * @license React
  * react-dom-test-utils.production.js
  *
--- layouts/Container/ContainerProps.d.ts
+++ layouts/Container/ContainerProps.d.ts
@@ -2,52 +2,52 @@ import { DivProps } from '../../components/Div';
 export default interface ContainerProps extends DivProps {
     isFluid?: boolean;
     gutter?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterXSmall?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterSmallOrLess?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterSmall?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterSmallOrMore?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterMediumOrLess?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterMedium?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterMediumOrMore?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterLargeOrLess?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterLarge?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterLargeOrMore?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
     gutterXLarge?: `${number}rem` | {
-        x?: `${number}rem`;
-        y?: `${number}rem`;
+        x?: `${number}rem` | number;
+        y?: `${number}rem` | number;
     };
 }
 //# sourceMappingURL=ContainerProps.d.ts.map
